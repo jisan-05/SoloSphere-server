34fcdc091e197d54b4165d0360bd6845c1d9f604
Tanvir <zishantanvir05@gmail.com>
2025-02-25 22:45:06 +0600

    delete a job from DB - added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 require('dotenv').config()
 
 const port = process.env.PORT || 9000
@@ -48,6 +48,14 @@ async function run() {
       const result = await jobsCollection.find(query).toArray()
       res.send(result)
 
+    })
+
+    // delete a job from db
+    app.delete('/job/:id', async (req,res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await jobsCollection.deleteOne(query)
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
