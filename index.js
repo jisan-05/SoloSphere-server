1f39e06adc241c5134f1cd0a4a1854b1dca0106b
Tanvir <zishantanvir05@gmail.com>
2025-03-11 19:41:24 +0600

    update bid status working . update method working

--- index.js
+++ index.js
@@ -84,12 +84,39 @@ async function run() {
 
         // get all bids for a specific user 
         app.get('/bids/:email', async(req,res)=>{
+            const isBuyer = req.query.buyer
             const email = req.params.email
-            const query = {email}
+            let query = {}
+            if(isBuyer){
+                query.buyer = email
+            }else{
+                query.email = email
+            }
             const result = await bidsCollection.find(query).toArray()
             res.send(result)
         })
 
+
+        // update bid status
+        app.patch('/bid-status-update/:id', async(req,res)=>{
+            const id = req.params.id
+            const {status} = req.body
+            const filter = {_id: new ObjectId(id)}
+            const updated = {
+                $set:{status: status}
+            }
+            const result = await bidsCollection.updateOne(filter,updated)
+            res.send(result)
+        })
+
+
+         // get all bid request for a specific user 
+        //  app.get('/bid-requests/:email', async(req,res)=>{
+        //     const email = req.params.email
+        //     const query = {buyer: email}
+        //     const result = await bidsCollection.find(query).toArray()
+        //     res.send(result)
+        // })
 
         // save a Bids in db
         app.post("/add-bid", async (req, res) => {
